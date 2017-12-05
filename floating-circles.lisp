3ab9ce85600acfcb98b2129040eec4793760863e
t-sin <shinichi.tanaka45@gmail.com>
2017-12-05 15:48:48 +0900

    Floating...

--- floating-circles.lisp
+++ floating-circles.lisp
@@ -7,7 +7,7 @@
 (defparameter +width+ 600)
 (defparameter +height+ 400)
 
-(defstruct pt x y vx vy h)
+(defstruct pt x y vx vy h cycle tick)
 
 (defun draw-point (p)
   (let ((x (pt-x p))
@@ -19,19 +19,26 @@
       (circle x y 1))))
 
 (defun move-point (p)
+  (setf (pt-vx p) (* (pt-vx p) 0.9891)
+        (pt-vy p) (* (pt-vy p) 0.9891))
   (incf (pt-x p) (pt-vx p))
   (incf (pt-y p) (pt-vy p))
-  (setf (pt-vx p) (* (pt-vx p) 0.9891)
-        (pt-vy p) (* (pt-vy p) 0.9891)))
+  (when (zerop (mod (pt-tick p) (pt-cycle p)))
+    (setf (pt-vx p) (- (random 2.0) 1)
+          (pt-vy p) (- (random 2.0) 1)
+          tick 0))
+  (incf (pt-tick p)))
 
 (let ((points (make-array 100)))
   (loop
      :for i :from 0 :below (length points)
      :do (setf (aref points i) (make-pt :x (random +width+)
                                         :y (random +height+)
-                                        :vx (- (random 4.0) 2)
-                                        :vy (- (random 4.0) 2)
-                                        :h (random 1.0))))
+                                        :vx (- (random 2.0) 1)
+                                        :vy (- (random 2.0) 1)
+                                        :h (random 1.0)
+                                        :tick 0
+                                        :cycle (+ 100 (random 100)))))
   (defsketch mysketch
       ((title "floating-circles")
        (width +width+)
